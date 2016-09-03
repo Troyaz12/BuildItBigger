c3d3a7382aaf3ce524c22a277526421b836a134e
Troy Gugler <guglert80@gmail.com>
2016-09-02 21:45:51 -0700

    added Interstitial add

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/Util.java
@@ -0,0 +1,16 @@
+package com.udacity.gradle.builditbigger;
+
+/**
+ * Created by TroysMacBook on 9/2/16.
+ */
+
+public class Util {
+
+    public boolean add = true;
+
+    public static boolean createAd() {
+
+
+        return true;
+    }
+}
